bd2709aab35000f0c1396df6cc5e844d790772d3
Gustavo Leyte-Vidal <glcdmx@gmail.com>
2023-05-16 11:30:57 -0400

    feat: fetch the data using provided api

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { type User } from './types.d'
 
 function App() {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=100')
+      .then(async (res) => await res.json())
+      .then((data) => {
+        setUsers(data.results)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
   return (
     <>
-      <div>Hello World!</div>
+      {users.map((user) => (
+        <div key={user.email}>{user.name.first}</div>
+      ))}
     </>
   )
 }
